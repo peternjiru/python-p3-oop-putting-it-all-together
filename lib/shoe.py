37b3fe35182f575becffdd43dc61e33d07ad4acc
Peter Njiru <peternjirum@gmail.com>
2023-07-23 19:36:14 -0400

    completes oop putting it all together lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand="Adidas", size=9):
+        self.brand = brand
+        self.size = size
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, brand):
+        self._brand = brand
+
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if (type(size) in (int, float)):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
